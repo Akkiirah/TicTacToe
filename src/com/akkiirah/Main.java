31508e95048338a10373d82aa91f7a257dd8577d
akkiirah <akkiirah@gmail.com>
2022-02-05 14:50:26 +0100

    Added a little tutorial at the beginning

--- src/com/akkiirah/Main.java
+++ src/com/akkiirah/Main.java
@@ -1,7 +1,9 @@
 package com.akkiirah;
 
+import java.util.concurrent.TimeUnit;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // Creates Map and Players
         Playfield map = new Playfield();
         Player player1 = new Player(1);
@@ -17,6 +19,9 @@ public class Main {
 
         // Initializes Map and Renders map once before the game starts
         map.InitializeMap();
+
+        StartGame();
+
         map.UpdateMap(' ', 0);
 
         // Cycles through until a winners has been decided
@@ -51,17 +56,34 @@ public class Main {
         EndGame(players);
     }
 
-    public static void EndGame(Player[] _players){
-        if(_players[0].winner)
+    // Some text explaining how to play the game
+    private static void StartGame() throws InterruptedException {
+        System.out.println("Welcome to Tic Tac Toe");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("The rules are:");
+        System.out.println("Player 1 has 'X', Player 2 has 'O'");
+        TimeUnit.SECONDS.sleep(4);
+        System.out.println("Each player enters a number ranging from 0 to 1 each turn");
+        System.out.println("They map is counted from top left to bottom right");
+        TimeUnit.SECONDS.sleep(4);
+        System.out.println("The goal is to get 3 of your symbol in a row - vertical, diagonal or horizontal");
+        TimeUnit.SECONDS.sleep(4);
+        System.out.println("Ready up! Good luck & have fun!");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("\n");
+    }
+
+    // Winner gets announced, game ends
+    private static void EndGame(Player[] _players){
+        if(_players[0].winner) {
             System.out.println("Player " +_players[0].number + " won the game!");
-
-        else if(_players[1].winner)
+        } else if(_players[1].winner){
             System.out.println("Player " +_players[1].number + " won the game!");
-
-
-        else
+        } else {
             System.out.println("No Winners! It's a draw!");
+        }
 
         System.out.println("Thank you for playing!");
     }
+
 }
